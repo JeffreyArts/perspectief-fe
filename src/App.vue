780d203966419bb066f9a9bc531b4c32ee89ee2c
Jeffrey Arts <sjeffff@gmail.com>
2023-05-13 17:39:03 +0200

    Remove socket io

--- src/App.vue
+++ src/App.vue
@@ -6,15 +6,12 @@
 
 <script setup lang="ts">
 import SiteFooter from "@/components/site-footer.vue"
-// import SocketIO from "@/stores/socketio"
 import LocalDB from "@/stores/localdb"
 import mobileCheck from "@/stores/is-mobile"
 
-// const socketIO = SocketIO()
 const isMobile = mobileCheck()
 const localDB = LocalDB()
 localDB.load()
-// socketIO.init()
 </script>
 
 <style src="./assets/scss/index.scss"></style>
