519fc2ed51ccf2f472a9a0eb18c6fc8381ef7e57
Jeffrey Arts <sjeffff@gmail.com>
2023-07-20 15:40:28 +0200

    Bugfix project

--- src/stores/page.ts
+++ src/stores/page.ts
@@ -1,7 +1,7 @@
 import { defineStore } from "pinia"
 import { ref} from "vue"
 
-export const Page = defineStore("isMobile", () => {
+export const Page = defineStore("page", () => {
     const step = 1
 
     return {
